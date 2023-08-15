311ee537c83713ed5ef2d5f143dd1565e9045cb1
Zaid Hasan <you@example.com>
2023-08-15 21:14:44 +0530

    Lab programs

--- BANKERAL.C
+++ BANKERAL.C
@@ -1,82 +1,75 @@
 #include <stdio.h>
-#include <stdlib.h>
-
 int main()
 {
-	int n,m,safe;
-	printf("Enter the number of process and resources respectively:\n");
-	scanf("%d",&n);
-	scanf("%d",&m);
-	int max[n][m],alloc[n][m],need[n][m];
-	int f[n],ans[n];
-	printf("For allocation:\n");
-	for(int i=0;i<n;i++)
+	// P0, P1, P2, P3, P4 are the Process names
+	int n, m, i, j, k;
+	n = 5; // Number of processes
+	m = 3; // Number of resources
+	int alloc[5][3] = {{0, 1, 0},
+					   {2, 0, 0},
+					   {3, 0, 2},
+					   {2, 1, 1},
+					   {0, 0, 2}};
+	int max[5][3] = {{7, 5, 3},
+					 {3, 2, 2},
+					 {9, 0, 2},
+					 {2, 2, 2},
+					 {4, 3, 3}};
+	int avail[3] = {3, 3, 2}; // Available Resources
+	 int f[n], ans[n], ind = 0;
+	for (k = 0; k < n; k++)
 	{
-		printf("Enter the given resource to the process P%d\n",i);
-		for(int j=0;j<m;j++)
+		f[k] = 0;
+	}
+	int need[n][m];
+	for (i = 0; i < n; i++)
+	{
+		for (j = 0; j < m; j++)
+			need[i][j] = max[i][j] -alloc[i][j];
+	}
+	int y = 0;
+	for (k = 0; k < 5; k++)
+	{
+		for (i = 0; i < n; i++)
 		{
-			printf("R[%d]: ",j);
-			scanf("%d",&alloc[i][j]);
-		}
-	}
-	printf("For Max:\n");
-	for(int i=0;i<n;i++)
-	{
-		printf("Enter the given resource to the process P%d\n",i);
-		for(int j=0;j<m;j++)
-		{
-			printf("R[%d]: ",j);
-			scanf("%d",&alloc[i][j]);
-		}
-	}
-	for(int i=0;i<n;i++)
-	{
-		for(int j=0;j<m;j++)
-		{
-			need[i][j]=max[i][j]-alloc[i][j];
-		}
-	}
-	for(int i=0;i<n;i++)
-	{
-		f[i]=0;
-	}
-	int avail[m];
-	printf("Enter the available resource\n");
-	for(int j=0;j<m;j++)
-	{
-		printf("R%d",j);
-		scanf("%d",&avail[j]);
-	}
-	for(int k=0;k<3;k++)
-	{
-		for(int i=0;i<n;i++)
-		{
-			if(f[i]==0)
+			if (f[i] == 0)
 			{
-				 int flag=0;
-				 for(int j=0;j<m;j++)
-				 {
-					if(need[i][j]>avail[j])
+				int flag = 0;
+				for (j = 0; j < m;j++)
+				{
+
+					if (need[i][j] > avail[j])
 					{
-						flag=1;
+						flag = 1;
 						break;
 					}
-				 }
-				 if(flag==0)
-				 {
-					ans[safe++]=i;
-					f[i]=1;
-					for(int j=0;j<m;j++)
-					{
-						avail[j] += need[i][j];
-					}
-				 }
+				}
+				if (flag == 0)
+				{
+					ans[ind++] = i;
+					for (y = 0; y < m; y++)
+						avail[y] += alloc[i][y];
+					f[i] = 1;
+				}
 			}
 		}
-
 	}
-
-
-
-
+	int flag = 1;
+	for (int i = 0; i < n; i++)
+	{
+		if (f[i] == 0)
+		{
+			flag = 0;
+			printf("The following systemis not safe");
+			break;
+		}
+	}
+	if (flag == 1)
+	{
+		printf("Following is the SAFE Sequence\n");
+		for (i = 0; i < n - 1; i++)
+		printf(" P%d ->", ans[i]);
+		printf(" P%d", ans[n - 1]);
+	}
+	return (0);
 }
---
+++ fcfsat.c
@@ -0,0 +1,64 @@
+#include <stdio.h>
+typedef struct fcfs
+{
+    int process; // Process Number
+    int burst;   // Burst Time
+    int arrival; // Arrival Time
+    float tat;   // Turn Around Time
+    float wt;    // Waiting Time
+} fcfs;
+int sort(fcfs[], int);
+int main()
+{
+    int n, i, temp = 0, AvTat = 0, AvWt = 0;
+    printf("Enter the number of processes: ");
+    scanf("%d", &n);
+    fcfs arr[n]; // Array of type fcfs
+    int tct[n];
+    for (i = 0; i < n; i++)
+    {
+        arr[i].process = i;
+        printf("Enter the process %d data\n", arr[i].process);
+        printf("Enter CPU Burst: ");
+        scanf("%d", &(arr[i].burst));
+        printf("Enter the arrival time: ");
+        scanf("%d", &(arr[i].arrival));
+    }
+    // Sorting the processes according to their arrival time
+    sort(arr, n);
+    printf("Process\t\tBurst Time\tArrival Time\tTurn Around Time\tWaiting Time\n");
+    for (i = 0; i < n; i++)
+    {
+        tct[i] = temp + arr[i].burst;
+        temp = tct[i];
+        arr[i].tat = tct[i] - arr[i].arrival;
+        arr[i].wt = arr[i].tat - arr[i].burst;
+        AvTat = AvTat + arr[i].tat;
+        AvWt = AvWt + arr[i].wt;
+        printf("%5d\t%15d\t\t%9d\t%6.3f\t\t%6.3f\n", arr[i].process, arr[i].burst, arr[i].arrival, arr[i].tat,
+               arr[i].wt);
+    }
+    printf("Average Turn Around Time: %f \nAverage Waiting Time: %f\n", AvTat / (float)n,
+           AvWt / (float)n);
+    return 0;
+}
+// Bubble Sort
+int sort(fcfs arr[], int n)
+{
+    int i, j;
+    fcfs k;
+    for (i = 0; i < n - 1; i++)
+    {
+        for (j = i + 1; j < n; j++)
+        {
+
+            // Sorting the processes according to their arrival time
+            if (arr[i].arrival > arr[j].arrival)
+            {
+                k = arr[i];
+                arr[i] = arr[j];
+                arr[j] = k;
+            }
+        }
+    }
+}
